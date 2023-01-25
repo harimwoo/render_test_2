41606a7f59212c87c786414d94a25dfae66e2a2d
harimwoo <harimwoo@gmail.com>
2023-01-25 16:01:29 -0500

    update port

--- backend/index.js
+++ backend/index.js
@@ -82,7 +82,7 @@ app.get('/api/notes', (request, response) => {
   
   app.use(unknownEndpoint)
   
-const PORT = 3001
-app.listen(PORT, () => {
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
